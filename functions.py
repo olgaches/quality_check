5d5dd0c665874d17ce6edbfe60289c667d7ed0a9
olgaches <chessnokova@hotmail.com>
2020-09-22 17:22:09 +0200

    update

--- functions.py
+++ functions.py
@@ -8,7 +8,8 @@ from arcpy import env
 import fnmatch
 import os
 import codecs
-
+from itertools import groupby
+from operator import itemgetter
 
 def check_media_folder(media_folder, client_defined_tables_columns, output_feedback, environment):
     """checks if any of the user-defined tables or columns appears in the media folder. XML files that have
@@ -51,15 +52,16 @@ def table_check(table_name, column_name, output_feedback, environment):
     return
 
 
-def columns_check(output_feedback, environment):
-    """checks if there are any user-defined columns in the environment defined as env.workspace"""
+def columns_check(output_feedback, environment, to_check_expression):
+    """checks if there are any user-defined columns in the environment defined as env.workspace
+    A check-expression can be for example 'u_', '_old', '_1', etc."""
 
     env.workspace = environment
 
     def columns(table):
         all_fields = arcpy.ListFields(table)
         for field in all_fields:
-            if 'u_' in str(field.name).lower():
+            if to_check_expression in str(field.name).lower():
                 values = [row[0] for row in arcpy.da.SearchCursor(table, field.name)]
                 if len(values) == 0 or str(set(values)) == 'set([None])':
                     output_feedback.writelines('column %s of the table %s is empty' % (field.name, table) + '\n')
@@ -68,7 +70,7 @@ def columns_check(output_feedback, environment):
                         'column %s of the table %s has the following unique values: ' % (field.name, table) + str(set(values)) + '\n')
         return
 
-    output_feedback.writelines('**The following is the check of the user-defined columns in the database**\n')
+    output_feedback.writelines('**The following is the check of the user-defined columns in the database (%s) **' % (to_check_expression) + '\n')
 
     tables = arcpy.ListTables()
     for table in tables:
@@ -81,4 +83,51 @@ def columns_check(output_feedback, environment):
         for feature in features:
             columns(feature)
 
+    return
+
+
+def knoten_auf_knoten(table_name, environment, output_feedback, field):
+    """checks if there are any geometrically overlapping points. For tables in the datasets the names of the dataset
+    has to be given in the environment, e.g. 'SEW/AWK_ABWASSERKNOTEN', field variable is the field that should be additionally checked,
+    for example ART_BAUWERK in AWK_ABWASSERKNOTEN"""
+    env.workspace = environment
+    table = environment + '/' + table_name
+    output_feedback.writelines(
+        '**The following is the check if there are any geometrically overlapping points in %s**: ' % (table_name) + '\n')
+
+    dic_data = {}
+    with arcpy.da.SearchCursor(table, ['OID@', 'SHAPE@XY', field]) as rows:
+        for row in rows:
+            dic_data[(row[0], row[2])] = row[1]
+
+    duplicates = []
+    for xy, objids in groupby(sorted(dic_data.items(), key=itemgetter(1)), itemgetter(1)):
+        all_objids = list(map(itemgetter(0), objids))
+        if len(all_objids) > 1:
+            output_feedback.writelines(str(all_objids) + ';' + str(xy) + '\n')
+            duplicates.append(all_objids)
+    print len(duplicates)
+    output_feedback.writelines('Number of duplicates in %s: ' % (table_name) + str(len(duplicates)) + '\n')
+    return
+
+def linie_auf_linie(table_name, environment, output_feedback):
+    """checks if there are any geometrically overlapping lines. For tables in the datasets the names of the dataset
+    has to be given in the environment, e.g. 'SEW/AWK_HALTUNG'"""
+    env.workspace = environment
+    table = environment + '/' + table_name
+    output_feedback.writelines('**The following is the check if there are any geometrically overlapping lines in %s**: ' % (table_name) + '\n')
+
+    dic_data = {}
+    with arcpy.da.SearchCursor(table, ['OID@', 'SHAPE@WKT']) as rows:
+        for row in rows:
+            dic_data[row[0]] = row[1]
+
+    duplicates = []
+    for xy, objids in groupby(sorted(dic_data.items(), key=itemgetter(1)), itemgetter(1)):
+        all_objids = list(map(itemgetter(0), objids))
+        if len(all_objids) > 1:
+            output_feedback.writelines(str(all_objids) + ';' + str(xy) + '\n')
+            duplicates.append(all_objids)
+    print len(duplicates)
+    output_feedback.writelines('Number of duplicates in %s: ' % (table_name) + str(len(duplicates)) + '\n')
     return
--- main_quality_check.py
+++ main_quality_check.py
@@ -15,6 +15,8 @@ import re
 from functions import table_check
 from functions import check_media_folder
 from functions import columns_check
+from functions import knoten_auf_knoten
+from functions import linie_auf_linie
 
 rootdir = "C:/data/SEW_Yverdon/SEW_Yverdon/"
 database_path = rootdir + "datasources/SEW_Yverdon_FGDB/db/SEW_Yverdon_FGDB.gdb/"
@@ -27,74 +29,105 @@ output_feedback = codecs.open(output_file, 'w', 'utf-8')
 ##checks how many user-defined tables and columns are in the database, and how many of the tables are secondary tables
 env.workspace = database_path
 tables = arcpy.ListTables()
-
-pattern = 'U_[A-Z]{3}S'
-
-client_defined_tables = []
-client_defined_lookup_tables = []
-client_defined_fields = []
-for table in tables:
-    if 'u_' in table.lower():
-        client_defined_tables.append(table)
-        if re.match(pattern, table):
-            client_defined_lookup_tables.append(table)
-    all_fields = arcpy.ListFields(table)
-    for field in all_fields:
-        if 'u_' in str(field.name).lower():
-            client_defined_fields.append(field.name)
-
-datasets = arcpy.ListDatasets()
-for dataset in datasets:
-    env.workspace = database_path + dataset
-    features = arcpy.ListFeatureClasses()
-    for table in features:
-        if 'u_' in table.lower():
-            client_defined_tables.append(table)
-            if re.match(pattern, table):
-                client_defined_lookup_tables.append(table)
-        all_fields = arcpy.ListFields(table)
-        for field in all_fields:
-            if 'u_' in str(field.name).lower():
-                client_defined_fields.append(field.name)
-
-output_feedback.writelines('Number of user-defined tables is: ' + str(len(client_defined_tables)) + '\n')
-output_feedback.writelines(
-    'Names of the user-defined tables are the following: ' + str(client_defined_tables) + '\n')
-
-output_feedback.writelines(
-    'Number of user-defined look up tables is: ' + str(len(client_defined_lookup_tables)) + '\n')
-output_feedback.writelines(
-    'Names of the user-defined look up tables are the following: ' + str(client_defined_lookup_tables) + '\n')
-
-client_defined_fields_set = set(client_defined_fields)
-
-output_feedback.writelines(
-    'Number of user-defined columns is: ' + str(len(client_defined_fields_set)) + '\n')
-output_feedback.writelines(
-    'Names of the user-defined columns are the following: ' + str(client_defined_fields_set) + '\n')
+#
+# pattern = 'U_[A-Z]{3}S'
+#
+# client_defined_tables = []
+# client_defined_lookup_tables = []
+# client_defined_fields = []
+# for table in tables:
+#     if 'u_' in table.lower():
+#         client_defined_tables.append(table)
+#         if re.match(pattern, table):
+#             client_defined_lookup_tables.append(table)
+#     all_fields = arcpy.ListFields(table)
+#     for field in all_fields:
+#         if 'u_' in str(field.name).lower():
+#             client_defined_fields.append(field.name)
+#
+# datasets = arcpy.ListDatasets()
+# for dataset in datasets:
+#     env.workspace = database_path + dataset
+#     features = arcpy.ListFeatureClasses()
+#     for table in features:
+#         if 'u_' in table.lower():
+#             client_defined_tables.append(table)
+#             if re.match(pattern, table):
+#                 client_defined_lookup_tables.append(table)
+#         all_fields = arcpy.ListFields(table)
+#         for field in all_fields:
+#             if 'u_' in str(field.name).lower():
+#                 client_defined_fields.append(field.name)
+#
+# output_feedback.writelines('Number of user-defined tables is: ' + str(len(client_defined_tables)) + '\n')
+# output_feedback.writelines(
+#     'Names of the user-defined tables are the following: ' + str(client_defined_tables) + '\n')
+#
+# output_feedback.writelines(
+#     'Number of user-defined look up tables is: ' + str(len(client_defined_lookup_tables)) + '\n')
+# output_feedback.writelines(
+#     'Names of the user-defined look up tables are the following: ' + str(client_defined_lookup_tables) + '\n')
+#
+# client_defined_fields_set = set(client_defined_fields)
+#
+# output_feedback.writelines(
+#     'Number of user-defined columns is: ' + str(len(client_defined_fields_set)) + '\n')
+# output_feedback.writelines(
+#     'Names of the user-defined columns are the following: ' + str(client_defined_fields_set) + '\n')
 
 ##--------------end general characteristics--------------
 
 ##--------------beginning functions--------------
 start_time = time.time()
 
-GN_lookup_check = table_check('GN_lookup', 'lookup_table', output_feedback, database_path)
+# GN_lookup_check = table_check('GN_lookup', 'lookup_table', output_feedback, database_path)
+#
+# GNREL_formula_check = table_check('GNREL_FORMULA', 'FORMULA', output_feedback, database_path)
+#
+# GNREL_rule_check = table_check('GNREL_RULE', 'FORMULA', output_feedback, database_path)
 
-GNREL_formula_check = table_check('GNREL_FORMULA', 'FORMULA', output_feedback, database_path)
+# client_defined_fields_tables = client_defined_fields_set.union(set(client_defined_tables))
+# print 'Length of the set for the media folder check', len(client_defined_fields_tables)
+#
+# ##--------------longer processes, comment out if not needed--------------
+# media_check = check_media_folder(media_folder, client_defined_fields_tables, output_feedback, database_path)
+# elapsed_time = time.time() - start_time
+# print 'Time after media_check', time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
 
-client_defined_fields_tables = client_defined_fields_set.union(set(client_defined_tables))
-print 'Length of the set for the media folder check', len(client_defined_fields_tables)
+# result_column_check = columns_check(output_feedback, database_path, 'u_')
+# elapsed_time = time.time() - start_time
+# print 'Time after column_check', time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
 
-##--------------longer processes, comment out if not needed--------------
-media_check = check_media_folder(media_folder, client_defined_fields_tables, output_feedback, database_path)
-elapsed_time = time.time() - start_time
-print 'Time after media_check', time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
-
-result_column_check = columns_check(output_feedback, database_path)
-elapsed_time = time.time() - start_time
-print 'Time after column_check', time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
+# result_column_check_old = columns_check(output_feedback, database_path, '_old')
+# elapsed_time = time.time() - start_time
+# print 'Time after column_check', time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
 
 ##--------------end functions--------------
 
 
+knoten_auf_knoten('SEW/AWK_ABWASSERKNOTEN', database_path, output_feedback, "ART_BAUWERK")
+# linie_auf_linie('SEW/AWK_HALTUNG', database_path, output_feedback)
 
+##--------------looking for specific words in the data--------------
+# tables = arcpy.ListTables()
+# for table in tables:
+#     print table
+#     all_fields = arcpy.ListFields(table)
+#     for field in all_fields:
+#         for row in arcpy.da.SearchCursor(table, field.name):
+#             if 'vorflutereinlauf' in str(row):
+#                 print table, field.name
+
+
+# environment = database_path
+# datasets = arcpy.ListDatasets()
+# for dataset in datasets:
+#     print dataset
+#     env.workspace = environment + dataset
+#     features = arcpy.ListFeatureClasses()
+#     for feature in features:
+#         all_fields = arcpy.ListFields(feature)
+#         for field in all_fields:
+#             for row in arcpy.da.SearchCursor(feature, field.name):
+#                 if 'vorflutereinlauf' in str(row):
+#                     print feature, field.name
